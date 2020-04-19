b284e2ae17de26c46af23c56b9f3f8fc16d8a7f9
glezo1 <danielgonzalezmarcos@hotmail.com>
2020-04-19 09:49:48 -0400

    Update beeper.py

--- beeper.py
+++ beeper.py
@@ -6,7 +6,6 @@ with contextlib.redirect_stdout(None):
     except: print('pygame required. Execute pip3 install pygame'); sys.exit(1)
 import time
 import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
 mixer.init()
 alert=mixer.Sound('./beep.wav')
